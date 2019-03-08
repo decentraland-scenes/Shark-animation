80be9238fb30a3f0ddec3649171343a1a03eb9b6
nearnshaw <nearnshaw@decentraland.org>
2019-03-08 18:58:08 -0300

    fix scenery size

--- src/game.ts
+++ src/game.ts
@@ -11,8 +11,8 @@ NOTE: when you try to get an animation clip that hasn't been created
 from a GLTFShape component, the clip is created automatically.
 */
 const animator = new Animator();
-let clipSwim = new AnimationState("swim")
-let clipBite = new AnimationState("bite")
+let clipSwim = new AnimationClip("swim")
+let clipBite = new AnimationClip("bite")
 animator.addClip(clipBite);
 animator.addClip(clipSwim);
 
@@ -33,6 +33,19 @@ engine.addEntity(shark)
 const seaBed = new Entity()
 seaBed.addComponent(new GLTFShape("models/Underwater.gltf"))
 seaBed.addComponent(new Transform({
-  position: new Vector3(10, 0, 10)
+  position: new Vector3(8, 0, 8),
+  scale: new Vector3(0.8, 0.8, 0.8)
 }))
 engine.addEntity(seaBed)
+
+
+// // Add 3D model for scenery
+// const seaBed = new Entity()
+// seaBed.addComponent(new GLTFShape("models/Underwater.gltf"))
+// seaBed.addComponent(new Transform({
+//   position: new Vector3(8, 0, 8),
+//   rotation: Quaternion.Euler(0, 270, 0),
+//   scale: new Vector3(0.8, 0.8, 0.8)
+
+// }))
+// engine.addEntity(seaBed)
