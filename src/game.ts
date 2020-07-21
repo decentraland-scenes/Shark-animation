edba6889dafe2eb0fffaa862376ff1790416faa8
Nicolas Earnshaw <nearnshaw@decentraland.org>
2020-07-21 15:15:31 -0300

    remove weight property from animations

--- src/game.ts
+++ src/game.ts
@@ -13,8 +13,8 @@ NOTE: when you try to get an animation clip that hasn't been created
 from a GLTFShape component, the clip is created automatically.
 */
 const animator = new Animator()
-let clipSwim = new AnimationState('swim', { weight: 0.9 })
-let clipBite = new AnimationState('bite', { weight: 0.01 })
+let clipSwim = new AnimationState('swim')
+let clipBite = new AnimationState('bite')
 animator.addClip(clipBite)
 animator.addClip(clipSwim)
 
